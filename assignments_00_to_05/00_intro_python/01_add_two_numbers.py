892a6a080d7f558069d9f63161c86a8c0e7ca9cb
Syed Abdul Sami <samiwpp@gmail.com>
2025-04-04 05:21:39 +0500

    Add to numbers

---
+++ assignments_00_to_05/00_intro_python/01_add_two_numbers.py
@@ -0,0 +1,11 @@
+"""
+Add 2 Numbers:
+This function sum two numbers.
+"""
+def main():
+    print("This program adds two numbers.")
+    first_num = int(input("Enter First Number: "))
+    second_num = int(input("Enter Second Number: "))
+    print(f"The sum of {first_num} and {second_num} is {first_num + second_num}.")
+if __name__ == '__main__':
+    main()
